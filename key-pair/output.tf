18dd867e81ed1f41fca409f25f24738c227831c9
nsd_rgeraldo <nsd_rgeraldo@uolinc.com>
2021-06-10 17:44:18 -0300

    Fix key pair output

--- key-pair/output.tf
+++ key-pair/output.tf
@@ -1,7 +1,7 @@
 output "id_key_pair_jumpox" {
-  value = aws_key_pair.jumpbox_key_pair.id
+  value = aws_key_pair.key_pair.id
 }
 
 output "name_key_pair_jumpox" {
-  value = aws_key_pair.jumpbox_key_pair.key_name
+  value = aws_key_pair.key_pair.key_name
 }
