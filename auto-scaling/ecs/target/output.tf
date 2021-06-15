e74e0fb13c5cb656522c393fbf4b31b705d0e58e
nsd_dfelgar <nsd_dfelgar@uolinc.com>
2021-06-15 08:12:45 -0300

    Fix target output return

--- auto-scaling/ecs/target/output.tf
+++ auto-scaling/ecs/target/output.tf
@@ -1,7 +1,7 @@
 output "ecs-autoscaling-target" {
   value = {
-    resource_id = aws_appautoscaling_target.appautoscaling_target.resource_id
-    scalable_dimension = aws_appautoscaling_target.appautoscaling_target.scalable_dimension
-    service_namespace = aws_appautoscaling_target.appautoscaling_target.service_namespace
+    resource_id = length(aws_appautoscaling_target.appautoscaling_target.resource_id) < 0 ? null : element(aws_appautoscaling_target.appautoscaling_target.resource_id, 0)
+    scalable_dimension = length(aws_appautoscaling_target.appautoscaling_target.scalable_dimension) < 0 ? null : element(aws_appautoscaling_target.appautoscaling_target.scalable_dimension, 0)
+    service_namespace = length(aws_appautoscaling_target.appautoscaling_target.service_namespace) < 0 ? null : element(aws_appautoscaling_target.appautoscaling_target.service_namespace, 0)
   }
 }
