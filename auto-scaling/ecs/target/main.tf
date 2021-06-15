d2181a5a30f08a8cd2ad34ed426138ed314b29d4
nsd_dfelgar <nsd_dfelgar@uolinc.com>
2021-06-14 22:05:54 -0300

    Create target

---
+++ auto-scaling/ecs/target/main.tf
@@ -0,0 +1,10 @@
+resource "aws_appautoscaling_target" "appautoscaling_target" {
+  count = var.auto_scalling_enable ? 1 : 0
+  max_capacity = var.max_capacity
+  min_capacity = var.min_capacity
+  resource_id = "service/${var.cluster_name}/${var.service_name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace = "ecs"
+
+  tags = var.tags
+}
---
+++ auto-scaling/ecs/target/variables.tf
@@ -0,0 +1,31 @@
+variable "auto_scalling_enable" {
+  type = bool
+  description = "Enable auto scalling resources"
+  default = true
+}
+
+variable "max_capacity" {
+  type = number
+  description = "Max tasks capacity"
+}
+
+variable "min_capacity" {
+  type = number
+  description = "Min tasks capacity"
+  default = 1
+}
+
+variable "cluster_name" {
+  type = string
+  description = "Cluster Name"
+}
+
+variable "service_name" {
+  type = string
+  description = "Service Name"
+}
+
+variable "tags" {
+  type = object({})
+  default = {}
+}
