bbd8ed9a9695b834589f2f639b33c0a2370360c7
nsd_dfelgar <nsd_dfelgar@uolinc.com>
2021-06-17 13:28:06 -0300

    SES template module

---
+++ ses/template/main.tf
@@ -0,0 +1,7 @@
+resource "aws_ses_template" "templates" {
+  count = length(var.templates)
+  name = var.templates[count.index].name
+  subject = var.from_file ? file(var.templates[count.index].subject) : var.templates[count.index].subject
+  html = var.from_file ? file(var.templates[count.index].html) : var.templates[count.index].html
+  text = var.from_file ? file(var.templates[count.index].text) : var.templates[count.index].text
+}
---
+++ ses/template/variable.tf
@@ -0,0 +1,15 @@
+variable "templates" {
+  type = list(object({
+    name = string
+    subject = string
+    html = string
+    text = string
+  }))
+  description = "Templates list"
+}
+
+variable "from_file" {
+  type = bool
+  description = "Load from file or input direct text"
+  default = true
+}
