510117718e0938de2f04961c23a848b74a56f533
nsd_dfelgar <nsd_dfelgar@uolinc.com>
2021-06-25 10:30:36 -0300

    add sg_access_id as output

--- elasticache/redis/output.tf
+++ elasticache/redis/output.tf
@@ -5,3 +5,7 @@ output "primary_endpoint_address" {
 output "reader_endpoint_address" {
   value = aws_elasticache_replication_group.redis_group.reader_endpoint_address
 }
+
+output "sg_access_id" {
+  value = var.create_security_groups ? module.elasticache_access_sg.*.sg_id[0] : null
+}
