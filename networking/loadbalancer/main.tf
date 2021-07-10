87d654fb407911d542d10829d06e0831d524e18b
nsd_dfelgar <nsd_dfelgar@uolinc.com>
2021-07-10 12:41:10 -0300

    Enable delete protection in loadbalancer

--- networking/loadbalancer/main.tf
+++ networking/loadbalancer/main.tf
@@ -4,7 +4,8 @@ resource "aws_lb" "lb" {
   load_balancer_type = var.type
   security_groups = var.sg_ids
   subnets = var.subnet_ids
-  enable_deletion_protection = false
+  enable_deletion_protection = var.deletion_protection
+
   tags = {
     Name = var.name
     environment = var.environment
--- networking/loadbalancer/variables.tf
+++ networking/loadbalancer/variables.tf
@@ -2,22 +2,27 @@ variable "name" {
   type = string
   description = "The name of the load balancer"
 }
+
 variable "internal" {
   type = bool
   description = "True if internal load balancer"
 }
+
 variable "type" {
   type = string
   description = "The type of the lb"
 }
+
 variable "sg_ids" {
   type = list(string)
   description = "The sg ids to be associated with the load balancer"
 }
+
 variable "subnet_ids" {
   type = list(string)
   description = "The ids of the subnets to the loadbalancer be within"
 }
+
 variable "vpc_id" {
   type = string
   description = "The vpc id"
@@ -25,4 +30,10 @@ variable "vpc_id" {
 
 variable "environment" {
   type = string
+}
+
+variable "deletion_protection" {
+  type = bool
+  default = false
+  description = "Enable Delete Protection"
 }
