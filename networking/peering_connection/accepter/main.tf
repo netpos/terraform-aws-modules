28f8e146fd8653007d5facacd876a4b61939575b
dfelgar <daniel@netpos.com.br>
2021-07-11 14:24:54 -0300

    Add route to VPC Peering accepter

--- networking/peering_connection/accepter/main.tf
+++ networking/peering_connection/accepter/main.tf
@@ -5,11 +5,11 @@ resource "aws_vpc_peering_connection_accepter" "peering_connection_accepter" {
   auto_accept = var.auto_accept
   tags = var.tags
 }
-
-resource "aws_route" "route" {
-  count = var.has_peer ? length(var.route_table_id) : 0
-
-  route_table_id = var.route_table_id[count.index]
-  destination_cidr_block = aws_vpc_peering_connection_accepter.peering_connection_accepter[0].requester[0].cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peering_connection_accepter[0].id
-}
+//
+//resource "aws_route" "route" {
+//  count = var.has_peer ? length(var.route_table_id) : 0
+//
+//  route_table_id = var.route_table_id[count.index]
+//  destination_cidr_block = aws_vpc_peering_connection_accepter.peering_connection_accepter[0].requester[0].cidr_block
+//  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peering_connection_accepter[0].id
+//}
--- networking/peering_connection/accepter/output.tf
+++ networking/peering_connection/accepter/output.tf
@@ -1,3 +1,7 @@
 output "requester" {
   value = var.has_peer ? aws_vpc_peering_connection_accepter.peering_connection_accepter.*.requester[0] : null
 }
+
+output "accepter" {
+  value = var.has_peer ? aws_vpc_peering_connection_accepter.peering_connection_accepter.*.accepter[0] : null
+}
