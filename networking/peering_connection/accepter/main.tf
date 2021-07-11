12631742b65f2c0ad0ec2d62b3e3a48c4a4a530b
dfelgar <daniel@netpos.com.br>
2021-07-11 14:20:54 -0300

    Add route to VPC Peering accepter

--- networking/peering_connection/accepter/main.tf
+++ networking/peering_connection/accepter/main.tf
@@ -10,6 +10,6 @@ resource "aws_route" "route" {
   count = var.has_peer ? length(var.route_table_id) : 0
 
   route_table_id = var.route_table_id[count.index]
-  destination_cidr_block = aws_vpc_peering_connection_accepter.peering_connection_accepter[0].requester[0].destination_cidr_block
+  destination_cidr_block = aws_vpc_peering_connection_accepter.peering_connection_accepter[0].requester[0].cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peering_connection_accepter[0].id
 }
