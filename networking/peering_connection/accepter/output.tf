49285053a6fce0e00840bad1344f1265d69cca50
dfelgar <daniel@netpos.com.br>
2021-07-11 14:31:12 -0300

    Add route to VPC Peering accepter

--- networking/peering_connection/accepter/output.tf
+++ networking/peering_connection/accepter/output.tf
@@ -1,7 +1,3 @@
 output "requester" {
-  value = var.has_peer ? aws_vpc_peering_connection_accepter.peering_connection_accepter.*.requester[0] : null
-}
-
-output "accepter" {
-  value = var.has_peer ? aws_vpc_peering_connection_accepter.peering_connection_accepter.*.accepter[0] : null
+  value = aws_vpc_peering_connection_accepter.peering_connection_accepter
 }
