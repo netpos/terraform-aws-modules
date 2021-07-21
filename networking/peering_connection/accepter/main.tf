d2e63018bc2ad8a47134434eeede16a960894e99
nsd_dfelgar <nsd_dfelgar@uolinc.com>
2021-07-21 15:45:33 -0300

    Remove count from peering connection module

--- networking/peering_connection/accepter/main.tf
+++ networking/peering_connection/accepter/main.tf
@@ -1,26 +1,21 @@
 resource "aws_vpc_peering_connection_accepter" "peering_connection_accepter" {
-  count = var.has_peer ? 1 : 0
-
   vpc_peering_connection_id = var.peer_id
   auto_accept = var.auto_accept
   tags = var.tags
 }
 
 data "aws_vpc_peering_connection" "peer_data" {
-  count = var.has_peer ? 1 : 0
   id = var.peer_id
 }
 
 data "aws_route_tables" "vpc_route_tables" {
-  count = var.has_peer ? 1 : 0
-
-  vpc_id = data.aws_vpc_peering_connection.peer_data.*.peer_vpc_id[0]
+  vpc_id = data.aws_vpc_peering_connection.peer_data.peer_vpc_id
 }
 
 resource "aws_route" "route" {
-  count = var.create_routes ? length(data.aws_route_tables.vpc_route_tables.*.ids) : 0
+  count = length(data.aws_route_tables.vpc_route_tables.ids)
 
-  route_table_id = data.aws_route_tables.vpc_route_tables.*.ids[count.index]
-  destination_cidr_block = data.aws_vpc_peering_connection.peer_data.*.cidr_block[0]
+  route_table_id = tolist(data.aws_route_tables.vpc_route_tables.ids)[count.index]
+  destination_cidr_block = data.aws_vpc_peering_connection.peer_data.cidr_block
   vpc_peering_connection_id = var.peer_id
 }
