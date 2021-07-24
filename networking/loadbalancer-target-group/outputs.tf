ddcd677f2def6379e3aa2fc413fc13098f6140ce
dfelgar <daniel@netpos.com.br>
2021-07-24 17:39:15 -0300

    Add suffix in arns in LB and TargetGroup

--- networking/loadbalancer-target-group/outputs.tf
+++ networking/loadbalancer-target-group/outputs.tf
@@ -2,3 +2,8 @@ output "target_group_arn" {
   value = concat(aws_lb_target_group.ip_target_group.*.arn, [
     null])[0]
 }
+
+output "target_group_arn_suffix" {
+  value = concat(aws_lb_target_group.ip_target_group.*.arn_suffix, [
+    null])[0]
+}
--- networking/loadbalancer/output.tf
+++ networking/loadbalancer/output.tf
@@ -2,6 +2,10 @@ output "lb_arn" {
   value = aws_lb.lb.arn
 }
 
+output "lb_arn_suffix" {
+  value = aws_lb.lb.arn_suffix
+}
+
 output "lb_name" {
   value = aws_lb.lb.name
 }
