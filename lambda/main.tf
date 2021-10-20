1d032b4d20f33b5df33b9fae272275ae057ad6a2
ironzep <renan_geraldo@yahoo.com.br>
2021-10-20 18:07:06 -0300

    Added AWS Lambda Vpc

--- lambda/main.tf
+++ lambda/main.tf
@@ -15,4 +15,13 @@ resource "aws_lambda_function" "aws_lambda_function" {
       variables = var.environment_variables
     }
   }
+
+  dynamic "vpc_config" {
+    for_each = var.vpc_subnet_ids != null && var.vpc_security_group_ids != null ? [true] : []
+    content {
+      security_group_ids = var.vpc_security_group_ids
+      subnet_ids         = var.vpc_subnet_ids
+    }
+  }
+
 }
--- lambda/variable.tf
+++ lambda/variable.tf
@@ -45,3 +45,15 @@ variable "handler" {
   type        = string
   default     = ""
 }
+
+variable "vpc_subnet_ids" {
+  description = "List of subnet ids when Lambda Function should run in the VPC. Usually private or intra subnets."
+  type        = list(string)
+  default     = null
+}
+
+variable "vpc_security_group_ids" {
+  description = "List of security group ids when Lambda Function should run in the VPC."
+  type        = list(string)
+  default     = null
+}
