8a9d10a481427793a472b845120041c3b9c3ff7a
ironzep <renan_geraldo@yahoo.com.br>
2021-10-21 13:54:27 -0300

    Added iam_policy

--- iam/iam-policy/variables.tf
+++ iam/iam-policy/variables.tf
@@ -1,35 +1,35 @@
 variable "create_policy" {
   description = "Whether to create the IAM policy"
-  type        = bool
-  default     = true
+  type = bool
+  default = true
 }
 
 variable "name" {
   description = "The name of the policy"
-  type        = string
-  default     = ""
+  type = string
+  default = ""
 }
 
 variable "path" {
   description = "The path of the policy in IAM"
-  type        = string
-  default     = "/"
+  type = string
+  default = "/"
 }
 
 variable "description" {
   description = "The description of the policy"
-  type        = string
-  default     = "IAM Policy"
+  type = string
+  default = "IAM Policy"
 }
 
 variable "policy" {
   description = "The path of the policy in IAM (tpl file)"
-  type        = string
-  default     = ""
+  type = string
+  default = ""
 }
 
 variable "tags" {
   description = "A map of tags to add to all resources."
-  type        = map(string)
-  default     = {}
+  type = map(string)
+  default = {}
 }
--- iam/iam-role/main.tf
+++ iam/iam-role/main.tf
@@ -1,4 +1,46 @@
+locals {
+  role_sts_externalid = flatten([
+    var.role_sts_externalid])
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    actions = var.trusted_role_actions
+
+    principals {
+      type = "AWS"
+      identifiers = var.trusted_role_arns
+    }
+
+    principals {
+      type = "Service"
+      identifiers = var.trusted_role_services
+    }
+
+    dynamic "condition" {
+      for_each = length(local.role_sts_externalid) != 0 ? [
+        true] : []
+      content {
+        test = "StringEquals"
+        variable = "sts:ExternalId"
+        values = local.role_sts_externalid
+      }
+    }
+  }
+}
+
 resource "aws_iam_role" "aws_iam_role" {
   name = var.name
   assume_role_policy = var.assume_role_policy
+
+  dynamic "inline_policy" {
+    for_each = var.inline_policies != null ? [
+      true] : []
+    content {
+      name = inline_policy.value["name"]
+      policy = inline_policy.value["policy"]
+    }
+  }
 }
