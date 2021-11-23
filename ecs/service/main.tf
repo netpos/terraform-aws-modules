8ca3983e36eb32095d229e5045073e6b30bb1eca
ironzep <renan_geraldo@yahoo.com.br>
2021-11-23 18:24:41 -0300

    Target type variables.tf

--- ecs/service/main.tf
+++ ecs/service/main.tf
@@ -1,12 +1,12 @@
 resource "aws_ecs_service" "ecs_service" {
   count = var.ecs_service_enabled ? 1 : 0
 
-  name = "${var.environment}-${var.project_name}"
-  cluster = var.cluster_id
-  task_definition = var.task_definition_arn
-  desired_count = var.min_capacity
+  name                 = "${var.environment}-${var.project_name}"
+  cluster              = var.cluster_id
+  task_definition      = var.task_definition_arn
+  desired_count        = var.min_capacity
   force_new_deployment = true
-  launch_type = var.launch_type
+  launch_type          = var.launch_type
   dynamic "capacity_provider_strategy" {
     for_each = var.capacity_provider_strategy
     content {
@@ -17,15 +17,14 @@ resource "aws_ecs_service" "ecs_service" {
   }
 
   health_check_grace_period_seconds = var.health_check_grace_period_seconds
-
   enable_execute_command = var.enable_execute_command
 
   dynamic "network_configuration" {
-    for_each = var.launch_type == "FARGATE" ? [1] : []
+    for_each = var.launch_type == "FARGATE" ? [true] : []
     content {
-      subnets = var.subnet_ids
+      subnets          = var.subnet_ids
       assign_public_ip = false
-      security_groups = var.service_sg
+      security_groups  = var.service_sg
     }
   }
 
@@ -39,8 +38,8 @@ resource "aws_ecs_service" "ecs_service" {
     for_each = local.lb_list
     content {
       target_group_arn = load_balancer.value["target_group_arn"]
-      container_name = load_balancer.value["container_name"]
-      container_port = load_balancer.value["container_port"]
+      container_name   = load_balancer.value["container_name"]
+      container_port   = load_balancer.value["container_port"]
     }
   }
 }
