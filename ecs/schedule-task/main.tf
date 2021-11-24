c8d6914e9d1f320303c7451fbfb33b6d2dc0ef9d
Daniel Felgar <nsd_dfelgar@uolinc.com>
2021-11-24 14:04:38 -0300

    Format Schedule task variables

--- ecs/schedule-task/main.tf
+++ ecs/schedule-task/main.tf
@@ -1,33 +1,32 @@
 resource "aws_cloudwatch_event_rule" "event_rule" {
   count = var.schedule_task_enabled ? 1 : 0
 
-  name = "${var.schedule_task_name}-rule"
+  name                = "${var.schedule_task_name}-rule"
   schedule_expression = var.event_rule_schedule_expression
-  description = var.event_rule_description
-  tags = var.tags
+  description         = var.event_rule_description
+  tags                = var.tags
 }
 
 resource "aws_cloudwatch_event_target" "ecs_scheduled_task_target" {
   count = var.schedule_task_enabled ? 1 : 0
 
-  rule = aws_cloudwatch_event_rule.event_rule[count.index].name
+  rule      = aws_cloudwatch_event_rule.event_rule[count.index].name
   target_id = "${var.schedule_task_name}-target"
-  arn = var.ecs_cluster_arn
-  role_arn = var.iam_cw_event_role_arn
+  arn       = var.ecs_cluster_arn
+  role_arn  = var.iam_cw_event_role_arn
 
   ecs_target {
-    group = "${var.schedule_task_name}-group"
-    launch_type = var.launch_type
-    platform_version = var.launch_type == "FARGATE" ? "LATEST" : null
-    task_count = var.ecs_task_count
+    group               = "${var.schedule_task_name}-group"
+    launch_type         = var.launch_type
+    platform_version    = var.launch_type == "FARGATE" ? "LATEST" : null
+    task_count          = var.ecs_task_count
     task_definition_arn = var.ecs_task_definition_arn
 
     dynamic "network_configuration" {
-      for_each = var.network_configuration == null ? [] : [
-        var.network_configuration]
+      for_each = var.network_configuration == null ? [] : [var.network_configuration]
       content {
-        subnets = network_configuration.value["subnets"]
-        security_groups = network_configuration.value["security_groups"]
+        subnets          = network_configuration.value["subnets"]
+        security_groups  = network_configuration.value["security_groups"]
         assign_public_ip = network_configuration.value["assign_public_ip"]
       }
     }
--- ecs/schedule-task/variables.tf
+++ ecs/schedule-task/variables.tf
@@ -1,62 +1,62 @@
 variable "schedule_task_enabled" {
-  type = bool
+  type        = bool
   description = "Enable schedule task"
 }
 
 variable "schedule_task_name" {
-  type = string
+  type        = string
   description = "Schedule task name"
 }
 
 variable "event_rule_schedule_expression" {
-  type = string
+  type        = string
   description = "Cron expression"
 }
 
 variable "event_rule_description" {
-  type = string
+  type        = string
   description = "Event rule description"
 }
 
 variable "tags" {
-  type = object({})
+  type        = object({})
   description = "Tags"
-  default = {}
+  default     = {}
 }
 
 variable "ecs_cluster_arn" {
-  type = string
+  type        = string
   description = "ECS Cluster ARN"
 }
 
 variable "iam_cw_event_role_arn" {
-  type = string
+  type        = string
   description = "Cloudwatch Event Role with ECS permission ARN"
 }
 
 variable "launch_type" {
-  type = string
+  type        = string
   description = "FARGATE or EC2"
-  default = "FARGATE"
+  default     = "FARGATE"
 }
 
 variable "ecs_task_count" {
-  type = number
+  type        = number
   description = "Task count"
 }
 
 variable "ecs_task_definition_arn" {
-  type = string
+  type        = string
   description = "Task arn"
 }
 
 variable "network_configuration" {
-  type = object({
-    subnets = list(string)
-    security_groups = list(string)
+  type        = object({
+    subnets          = list(string)
+    security_groups  = list(string)
     assign_public_ip = bool
   })
   description = "Network configuration if network mode is awsvpc"
-  default = null
+  default     = null
 }
 
