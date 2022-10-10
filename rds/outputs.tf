c784dccd7675cecd13cb6d093a38965ffaca2799
ironzep <renan_geraldo@yahoo.com.br>
2022-10-10 05:05:31 -0300

    Outputing sg id

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -6,6 +6,10 @@ output "rds_sg_access_id" {
   value = module.db_access_sg.sg_id
 }
 
+output "rds_sg_id" {
+  value = module.rds_sg.sg_id
+}
+
 output "rds_arn" {
   value = aws_db_instance.rds.arn
 }
