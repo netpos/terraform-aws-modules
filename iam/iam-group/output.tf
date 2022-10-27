fc2c2d02586ed374efae594b58c156fddac350ef
Daniel Felgar <danielsrf0203@gmail.com>
2022-10-27 09:24:51 -0300

    Add users and group outputs

--- iam/iam-group/output.tf
+++ iam/iam-group/output.tf
@@ -1,3 +1,7 @@
 output "group_id" {
   value = aws_iam_group.iam-group.id
 }
+
+output "group_name" {
+  value = aws_iam_group.iam-group.name
+}
--- iam/iam-user/output.tf
+++ iam/iam-user/output.tf
@@ -1,3 +1,7 @@
 output "user_id" {
   value = aws_iam_user.iam-user.id
 }
+
+output "user_name" {
+  value = aws_iam_user.iam-user.name
+}
