29c0a9e8e8b29fa4d07206d1cf3e48cf21ff846b
Daniel Felgar <danielsrf0203@gmail.com>
2022-11-11 16:47:42 -0300

    Add ecs lifecycle policy

--- ecs/ecr/main.tf
+++ ecs/ecr/main.tf
@@ -1,8 +1,18 @@
 resource "aws_ecr_repository" "ecr_repository" {
   name = var.name
+
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+
+  tags = var.tags
+}
+
+resource "aws_ecr_lifecycle_policy" "ecr_repository_lifecycle_policy" {
+  count = var.lifecycle_policy == null ? 0 : 1
+
+  repository = aws_ecr_repository.ecr_repository.name
+  policy     = var.lifecycle_policy
 }
--- ecs/ecr/variables.tf
+++ ecs/ecr/variables.tf
@@ -1,3 +1,16 @@
 variable "name" {
   description = "The name of the repository"
+  type        = string
+}
+
+variable "tags" {
+  description = "tags"
+  type        = object({})
+  default     = {}
+}
+
+variable "lifecycle_policy" {
+  description = "Lifecycle policy"
+  type        = string
+  default     = null
 }
