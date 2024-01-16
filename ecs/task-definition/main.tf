6c03d58d03ddd1028006cd9c354cbe75015bfdc2
Daniel Felgar <danielsrf0203@gmail.com>
2024-01-16 12:32:10 -0300

    Add tags in task-definition modules

--- ecs/task-definition/main.tf
+++ ecs/task-definition/main.tf
@@ -26,14 +26,15 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   requires_compatibilities = local.requires_compatibilities
   network_mode             = var.network_mode
   cpu                      = var.task_definition.cpu
+
+  tags = var.tags
 }
 
 resource "aws_cloudwatch_log_group" "cloudwatch_log_group" {
   name              = var.log_group_name
   retention_in_days = var.retention_in_days
-  tags              = {
-    Environment = var.environment
-  }
+
+  tags = var.tags
 }
 
 locals {
